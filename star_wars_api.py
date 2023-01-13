8d631547212b3ea312ba63e54525658989664676
Burns <BurnsG@moodys.com>
2023-01-13 12:53:37 +0000

    print names in order

--- star_wars_api.py
+++ star_wars_api.py
@@ -1,7 +1,19 @@
 import requests
 import pprint
+import json
 
 pp = pprint.PrettyPrinter(indent=4)
 
-response = requests.get("https://swapi.dev/api/people/1/")
-pprint.pprint(response.text)
+response = requests.get("https://swapi.dev/api/starships/")
+data = response.json()
+pprint.pprint(data)
+
+names = []
+
+for i in data['results']:
+    names.append(i['name'])
+
+names.sort()
+
+print(names)
+
