38f764b949f252c37afc233bcc56804295f7105d
Burns <BurnsG@moodys.com>
2023-01-18 08:15:18 -0500

    update

--- star_wars_api.py
+++ star_wars_api.py
@@ -12,8 +12,15 @@ app.config['SECRET_KEY'] = '#$%^&*'
 response = requests.get("https://swapi.dev/api/starships/")
 data = response.json()
 
-#data = session.get('data')
+#loop that calls endpoint until it's done, shove everything in dictionary 
+data["next"]
 
+# having an attribute different than what you sort by could be easier 
+# named tuples (could use for headers in the 4 loop)
+
+
+#data = session.get('data')
+print()
 
 #Here, I wanted the user to be able to sort using any key. since I don't know if each dictionary has the exact same keys, I'll iterate through all the dictionaries in the response and add each key to the list if it does not exist in the list already
 starship_keys = []
@@ -48,25 +55,17 @@ def starships():
         sort_value = request.form["starship_value"]
         sort_order = request.form["sort_by"]
 
-
         if sort_order == "Ascending":
-            newlist = sorted(data["results"], key=itemgetter(f'{sort_value}')) 
+            newlist = sorted(data["results"], key=itemgetter(f'{sort_value}'))
         elif sort_order == "Descending":
             newlist = sorted(data["results"], key=itemgetter(f'{sort_value}'), reverse = True)     
-       # else:
-        #    "sorry, your input was not valid. Please enter either ascending or descending."
-        #    sort_order = input()
-        #    if sort_order == "ascending":
-        #        newlist = sorted(data["results"], key=itemgetter(f'{sort_order}')) 
-        #    elif sort_order == "descending":
-        #        newlist = sorted(data["results"], key=itemgetter(f'{sort_order}'), reverse = True)   
 
         names = [ item['name'] for item in newlist]
         values = [ item[f'{sort_value}'] for item in newlist]
 
         sorted_list= merge(names, values)
 
-        return render_template("results.html", sorted_list=sorted_list)
+        return render_template("index.html", sorted_list=sorted_list, starship_keys=starship_keys, sort_value=sort_value)
     #print(names)
 
 if __name__ == '__main__':
